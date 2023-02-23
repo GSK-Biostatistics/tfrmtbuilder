85e4b1b068a0736851ef337fd62dd1cfe4515be6
karima662 <72526731+karima662@users.noreply.github.com>
2023-02-23 19:29:15 +0000

    add css for navlinks in side_panel class

--- R/tfrmtbuilder_ui.R
+++ R/tfrmtbuilder_ui.R
@@ -24,29 +24,32 @@ tfrmtbuilder_ui <- function(id){
                  fluidPage(
                    fluidRow(
                      column(7,
-                            navlistPanel(
-                              id = ns("tabs"),
-                              tabPanel("Overview",
-                                       fluidPage(
-                                         fluidRow(
-                                        #   column(5,
-                                                  datamapping_ui(ns("overview")) #),
-                                        #   column(7,
-                                        #          h3("Data") ,
-                                        # DTOutput(ns("data_view"))
-                                        #           )
-                                          )
-                                       )),
-                              tabPanel("Body Plan", body_plan_ui(ns("body_plan"))),
-                              tabPanel("Row Group Plan", row_grp_plan_ui(ns("row_grp_plan"))),
-                              tabPanel("Column Plan", col_plan_ui(ns("col_plan"))),
-                              tabPanel("Column Style Plan", col_style_plan_ui(ns("col_style_plan"))),
-                              tabPanel("Footnote Plan", footnote_plan_ui(ns("footnote_plan"))),
-                              tabPanel("Big Ns", big_n_ui(ns("big_n"))),
-                              widths = c(3, 9),
-                              well = FALSE
-                            )
-                     )  ,
+                            tags$div(
+                              class = "side_panel",
+                              navlistPanel(
+                                id = ns("tabs"),
+                                tabPanel("Overview",
+                                         fluidPage(
+                                           fluidRow(
+                                             # column(5,
+                                                    datamapping_ui(ns("overview")) #),
+                                             # column(7,
+                                             #        h3("Data") ,
+                                             #        DTOutput(ns("data_view"))
+                                             #      )
+                                           )
+                                         )),
+                                tabPanel("Body Plan", body_plan_ui(ns("body_plan"))),
+                                tabPanel("Row Group Plan", row_grp_plan_ui(ns("row_grp_plan"))),
+                                tabPanel("Column Plan", col_plan_ui(ns("col_plan"))),
+                                tabPanel("Column Style Plan", col_style_plan_ui(ns("col_style_plan"))),
+                                tabPanel("Footnote Plan", footnote_plan_ui(ns("footnote_plan"))),
+                                tabPanel("Big Ns", big_n_ui(ns("big_n"))),
+                                widths = c(3, 9),
+                                well = FALSE
+                                )
+                              )
+                            ),
                      column(5,
                             div(id = ns("sidebar"),
                                 table_view_ui(ns("tbl_view"))

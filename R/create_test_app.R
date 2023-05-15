b2cdb5861224e1564f1a5271672380e98ceb7f80
Becca Krouse <becca.z.krouse@gsk.com>
2023-05-15 13:16:13 -0400

    helper function to run a module as standalone app

---
+++ R/create_test_app.R
@@ -0,0 +1,24 @@
+
+#' Helper function for testing modules
+#'
+#' @param mod_name Module function name prefix (without `_ui` or `_server`)
+#' @param args_ui named list of arguments/values to pass to the UI function
+#' @param args_server named list of arguments/values to pass to the server function
+#'
+#' @noRd
+test_module <- function(mod_name, args_ui = list(), args_server = list()) {
+
+  id <- "mod_id"
+
+  shinyApp(
+    fluidPage(
+      do.call(paste0(mod_name, "_ui"), c(id = id, args_ui))
+    ),
+    function(input,output,session){
+
+      vals <- do.call(paste0(mod_name, "_server"), c(id = id, args_server))
+
+      exportTestValues(vals = {vals()})
+    }
+  )
+}

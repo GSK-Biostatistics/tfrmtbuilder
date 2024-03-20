5851f6349e3bf29b1a9ccf1a751e1f8ffe943d2a
Becca Krouse <becca.z.krouse@gsk.com>
2024-03-20 09:39:55 -0400

    css updates

--- R/tfrmtbuilder_ui.R
+++ R/tfrmtbuilder_ui.R
@@ -20,90 +20,85 @@ tfrmtbuilder_ui <- function(id){
                          primary = "#254988",
                          bg = "#ffffff",
                          fg = "#000000"
-                         ) ,
+        ) ,
         id = ns("all_tabs"),
         nav_panel("Home", home_ui(ns("home"))),
         nav_panel("Initialize", load_ui(ns("load"))),
         nav_panel("Edit",
-                 page_fillable(
-                   layout_sidebar(
-                              navset_pill_list(
-                                id = ns("tabs"),
+                  page_fillable(
+                    layout_sidebar(
+                      navset_pill_list(
+                        id = ns("tabs"),
 
-                                nav_panel(div( h6("Data Mapping", class = "zero_margin"),
-                                              div("(Required)", id = "tab_note")),
-                                         value = "Data Mapping",
-                                           div(style = "height: 650px",
-                                             datamapping_ui(ns("overview"))
-                                           , id = "content_border")
-                                         ),
+                        nav_panel(div( h6("Data Mapping", class = "zero_margin"),
+                                       div("(Required)", id = "tab_note")),
+                                  value = "Data Mapping",
+                                  div(style = "height: 650px",
+                                      datamapping_ui(ns("overview"))
+                                      , id = "content_border")
+                        ),
 
-                                nav_panel(div( h6("Body Plan", class = "zero_margin"),
-                                              div("(Required)", id = "tab_note")),
-                                         div( body_plan_ui(ns("body_plan")), id = "content_border")),
+                        nav_panel(div( h6("Body Plan", class = "zero_margin"),
+                                       div("(Required)", id = "tab_note")),
+                                  div( body_plan_ui(ns("body_plan")), id = "content_border")),
 
-                                nav_panel(div( h6("Row Group Plan", class = "zero_margin"),
-                                              div("(Optional)", id = "tab_note")),
-                                         div( row_grp_plan_ui(ns("row_grp_plan")), id = "content_border")),
+                        nav_panel(div( h6("Row Group Plan", class = "zero_margin"),
+                                       div("(Optional)", id = "tab_note")),
+                                  div( row_grp_plan_ui(ns("row_grp_plan")), id = "content_border")),
 
-                                nav_panel(div( h6("Column Plan", class = "zero_margin"),
-                                            div("(Optional)", id = "tab_note")),
-                                         value = "Column Plan",
-                                         div( col_plan_simple_ui(ns("col_plan")), id = "content_border")),
+                        nav_panel(div( h6("Column Plan", class = "zero_margin"),
+                                       div("(Optional)", id = "tab_note")),
+                                  value = "Column Plan",
+                                  div( col_plan_simple_ui(ns("col_plan")), id = "content_border")),
 
-                                nav_panel(div( h6("Column Style Plan", class = "zero_margin"),
-                                              div("(Optional)", id = "tab_note")),
-                                         div( col_style_plan_ui(ns("col_style_plan")), id = "content_border")),
+                        nav_panel(div( h6("Column Style Plan", class = "zero_margin"),
+                                       div("(Optional)", id = "tab_note")),
+                                  div( col_style_plan_ui(ns("col_style_plan")), id = "content_border")),
 
-                                nav_panel(div( h6("Footnote Plan", class = "zero_margin"),
-                                              div("(Optional)", id = "tab_note")),
-                                         div( footnote_plan_ui(ns("footnote_plan")), id = "content_border")),
+                        nav_panel(div( h6("Footnote Plan", class = "zero_margin"),
+                                       div("(Optional)", id = "tab_note")),
+                                  div( footnote_plan_ui(ns("footnote_plan")), id = "content_border")),
 
-                                nav_panel(div( h6("Page Plan", class = "zero_margin"),
-                                              div("(Optional)", id = "tab_note")),
-                                         div( page_plan_ui(ns("page_plan")), id = "content_border")),
+                        nav_panel(div( h6("Page Plan", class = "zero_margin"),
+                                       div("(Optional)", id = "tab_note")),
+                                  div( page_plan_ui(ns("page_plan")), id = "content_border")),
 
-                                nav_panel(div( h6("Big Ns", class = "zero_margin"),
-                                              div("(Optional)", id = "tab_note")),
-                                         div( big_n_ui(ns("big_n")), id = "content_border")),
+                        nav_panel(div( h6("Big Ns", class = "zero_margin"),
+                                       div("(Optional)", id = "tab_note")),
+                                  div( big_n_ui(ns("big_n")), id = "content_border")),
 
-                                nav_panel(div( h6("Titles", class = "zero_margin"),
-                                              div("(Optional)", id = "tab_note")),
-                                         div( titles_ui(ns("titles")), id = "content_border")),
+                        nav_panel(div( h6("Titles", class = "zero_margin"),
+                                       div("(Optional)", id = "tab_note")),
+                                  div( titles_ui(ns("titles")), id = "content_border")),
 
-                                widths = c(3, 9),
-                                well = FALSE
-                             #   )
-                              ),
-                           sidebar = sidebar(
-                             card(
-                                   accordion(
-                                   tabsetPanel(
-                                     tabPanel(title = div("Table", class = "tab_names"), br(),
-                                              table_outer_ui(ns("tbl_view"))),
-                                     tabPanel(title = div("Data", class = "tab_names"),
-                                              DTOutput(ns("data_view")))
-                                   )
-                                 ),
-                                 height="650px"
-                                 ),
-                                 position = "right",
-                            width="50%"
-                                )
-                            )
-                     )
+                        widths = c(3, 9),
+                        well = FALSE
+                      ),
+                      sidebar = sidebar(
+                        navset_tab(
+                          nav_panel(title = "Table", br(),
+                                    table_outer_ui(ns("tbl_view"))),
+                          nav_panel(title = "Data",
+                                    DTOutput(ns("data_view")))
+                        ),
+                        position = "right",
+
+                        width="40%"
+                      )
+                    )
+                  )
         ) ,
         nav_panel("Export", export_ui(ns("export"))),
         header = tags$script(HTML(paste0("var header = $('.navbar> .container-fluid');",
-                             "header.append('<div style=\"float:right; color:#fff;\">",
-                             "<div id =\"mocktoggle\" class=\"form-group shiny-input-container\"",
-                             "data-require-bs-version=\"5\" ",
-                             "data-require-bs-caller=\"input_switch()\">",
-                             " <div class=\"bslib-input-switch form-switch form-check\">",
-                             " <input id=\"", ns("mockmode"), "\" class=\"form-check-input\" type=\"checkbox\" role=\"switch\"/>",
-                             "<label class=\"form-check-label\" for=\"mockmode\">",
-                             "<span>Mock Mode</span>  </label>  </div></div>",
-                             "</div>');")))
+                                         "header.append('<div style=\"float:right; color:#fff;\">",
+                                         "<div id =\"mocktoggle\" class=\"form-group shiny-input-container\"",
+                                         "data-require-bs-version=\"5\" ",
+                                         "data-require-bs-caller=\"input_switch()\">",
+                                         " <div class=\"bslib-input-switch form-switch form-check\">",
+                                         " <input id=\"", ns("mockmode"), "\" class=\"form-check-input\" type=\"checkbox\" role=\"switch\"/>",
+                                         "<label class=\"form-check-label\" for=\"mockmode\">",
+                                         "<span>Mock Mode</span>  </label>  </div></div>",
+                                         "</div>');")))
       )
 
     )
